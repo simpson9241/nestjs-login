7a73636f1dab47ae0783d04d8b3527e16e9c0b4b
simpson9241 <simpson9241@gmail.com>
2022-07-08 17:21:13 +0900

    added updateUser, deleteUser func

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,8 @@
-import { ForbiddenException, HttpStatus, Injectable } from '@nestjs/common';
+import { ForbiddenException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { createDecipheriv } from 'crypto';
-import { create } from 'domain';
 import { Repository } from 'typeorm';
 import { CreateUserDTO } from './dto/create-user.dto';
+import { UpdateUserDTO } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
 
 @Injectable()
@@ -11,32 +10,44 @@ export class UserService {
     constructor(
         @InjectRepository(User)
         private userRepository: Repository<User>,
-    ){}
+    ) { }
 
-    async createUser(createUserDTO:CreateUserDTO){
-        const isExist = await this.userRepository.findOne({where:{userID:createUserDTO.userID}});
-        if (isExist){
-            throw new ForbiddenException({
-                statusCode: HttpStatus.FORBIDDEN,
-                message: ['User already exists'],
-                error: 'Forbidden'
-            })
+    async createUser(createUserDTO: CreateUserDTO) {
+        const isExist = await this.userRepository.findOne({ where: { userID: createUserDTO.userID } });
+        if (isExist) {
+            throw new ForbiddenException('User Already Exists');
         }
 
-        const {password, ...result} = await this.userRepository.save(createUserDTO);
+        const { password, ...result } = await this.userRepository.save(createUserDTO);
         return result;
     }
 
-    async findAll(){
+    async findAll() {
         return this.userRepository.find({
-            select: ["seq","userID","userName","role"],
+            select: ["seq", "userID", "userName", "role"],
         });
     }
 
-    findOne(id: string){
-        return this.userRepository.findOne({
-        where:{userID: id},
-        select: ["seq","userID","userName","role"],}
+    async findOne(id: string) {
+        const user = this.userRepository.findOne({
+            where: { userID: id },
+            select: ["seq", "userID", "userName", "role"],
+        }
         );
+        if (!user) {
+            throw new NotFoundException(`User ${id} not Found`);
+        }
+        return user;
+    }
+
+    async updateUser(id: string, updateUserDTO: UpdateUserDTO) {
+        await this.userRepository.update(
+            { userID: id },
+            updateUserDTO
+        );
+    }
+
+    async deleteUser(id: string) {
+        await this.userRepository.delete({ userID: id });
     }
 }
