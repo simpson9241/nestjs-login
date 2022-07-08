fdd359000a785e709522ff396ea8cc462dab45e2
simpson9241 <simpson9241@gmail.com>
2022-07-08 17:20:46 +0900

    changed test module to use test db, added tests

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,69 +1,119 @@
+import { ForbiddenException, NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import { getRepositoryToken, TypeOrmModule } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CreateUserDTO } from './dto/create-user.dto';
+import { UpdateUserDTO } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
 import { UserService } from './user.service';
 
-export type MockType<T> = {
-  [P in keyof T]?: jest.Mock<{}>;
-};
-
-export const repositoryMockFactory:() => MockType<Repository<any>> = jest.fn(()=>({
-  findOne: jest.fn(),
-  find: jest.fn(),
-  save: jest.fn(),
-}));
-
 describe('UserService', () => {
-  let service: UserService;
-  let repositoryMock: MockType<Repository<User>>;
-  beforeEach(async () => {
-    const module = await Test.createTestingModule({
-      providers: [
-        UserService,
-        {
-          provide:getRepositoryToken(User),
-          useFactory:repositoryMockFactory
-        }
-      ],
-    }).compile();
+  let userService: UserService;
+  let module: TestingModule;
 
-    service = module.get<UserService>(UserService);
-    repositoryMock = module.get(getRepositoryToken(User));
+  beforeEach(async () => {
+    module = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forFeature([User]),
+        TypeOrmModule.forRoot({
+          type: 'mysql',
+          host: 'localhost',
+          port: 3316,
+          username: 'admin',
+          password: 'admin',
+          database: 'test',
+          entities: [User],
+          synchronize: true,
+        })
+      ],
+      providers: [UserService]
+    }).compile();
+    userService = module.get<UserService>(UserService);
+  });
+
+  afterEach(async () => {
+    await module.close();
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(userService).toBeDefined();
   });
 
-  // describe("createUser")
+  describe('createUser', () => {
+    it("should create an user", async () => {
+      const createUserDTO: CreateUserDTO = {
+        userID: "dummy8",
+        userName: "dummy8",
+        password: "test123",
+        role: "test"
+      };
+      const result = await userService.createUser(createUserDTO);
+      expect(result).toBeDefined();
+      expect(result).toBeInstanceOf(Object);
+      console.log(result);
+    });
 
-  describe("findAll",()=>{
-    it("should return an array",async ()=>{
-      const users = [
-        {seq: 1, userName : "test1", userID: "test1", password: "test" , "role": "test"},
-        {seq: 2, userName : "test2", userID: "test2", password: "test" , "role": "test"},
-        {seq: 3, userName : "test3", userID: "test3", password: "test" , "role": "test"}
-      ];
-      repositoryMock.find.mockImplementation((select:Object)=>{
-        const returned_users = users;
-        returned_users.forEach(object =>{
-          delete object['password'];
-        });
-        return returned_users;
-      });
-      console.log(await service.findAll());
-      expect(await service.findAll()).toBeInstanceOf(Array);
-
+    it("should throw a Forbidden Exception", async () => {
+      const createUserDTO: CreateUserDTO = {
+        userID: "dummy8",
+        userName: "dummy8",
+        password: "test123",
+        role: "test"
+      };
+      try {
+        await userService.createUser(createUserDTO);
+      } catch (e) {
+        expect(e).toBeInstanceOf(ForbiddenException);
+        expect(e.message).toEqual('User Already Exists');
+      }
     });
   });
 
-  describe("findOne",()=>{
-    it("should return a user",()=>{
-      const user = {seq: 1, userName : "test", userID: "test", password: "test" , "role": "test"};
-      repositoryMock.findOne.mockReturnValue(user);
-      expect(service.findOne(user.userID)).toEqual(user);
+  describe('findAll', () => {
+    it("should return array of users", async () => {
+      const users = await userService.findAll();
+      expect(users).toBeInstanceOf(Array);
+      console.log(users);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return an user', async () => {
+      const user = await userService.findOne('dummy8');
+      expect(user).toBeDefined();
+      console.log(user);
+    });
+
+    it('should return throw NotFoundException', async () => {
+      try {
+        await userService.findOne('unvalid');
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual("User unvalid Not Found");
+      }
     })
   });
+
+  describe('updateUser', () => {
+    it('should update an user', async () => {
+      const updateUserDTO: UpdateUserDTO = {
+        userID: "dummy",
+        userName: "dummy",
+        password: "test",
+        role: "test"
+      };
+      await userService.updateUser(
+        'dummy',
+        updateUserDTO
+      );
+      console.log(await userService.findOne('dummy'));
+    })
+  });
+
+  describe('deleteUser', () => {
+    it('should delete an user', async () => {
+      await userService.deleteUser('dummy8')
+    });
+  });
+
 
 });
