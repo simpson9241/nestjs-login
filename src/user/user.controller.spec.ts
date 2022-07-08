7a6f45bd67b9bbb4c96147b02b4d3edbe0042dbd
simpson9241 <simpson9241@gmail.com>
2022-07-09 04:19:07 +0900

    added updateUser(), deleteUser()

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,18 +1,122 @@
+import { ForbiddenException, NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CreateUserDTO } from './dto/create-user.dto';
+import { UpdateUserDTO } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 import { UserController } from './user.controller';
+import { UserService } from './user.service';
 
 describe('UserController', () => {
-  let controller: UserController;
+  let userController: UserController;
+  let userService: UserService;
+  let module: TestingModule;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    module= await Test.createTestingModule({
+      imports:[
+        TypeOrmModule.forFeature([User]),
+        TypeOrmModule.forRoot({
+          type: 'mysql',
+          host: 'localhost',
+          port: 3316,
+          username: 'admin',
+          password: 'admin',
+          database: 'test',
+          entities: [User],
+          synchronize: true,
+        })
+      ],
       controllers: [UserController],
+      providers: [UserService]
     }).compile();
 
-    controller = module.get<UserController>(UserController);
+    userController = module.get<UserController>(UserController);
+    userService = module.get<UserService>(UserService);
   });
 
+  afterEach(async () =>{
+    await module.close();
+  })
+
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(userController).toBeDefined();
+  });
+
+  describe('create',()=>{
+    it('should create an user', async()=>{
+      const createUserDTO: CreateUserDTO = {
+        userID: "dummy10",
+        userName: "dummy10",
+        password: "test123",
+        role: "test"
+      };
+      const result = await userController.create(createUserDTO);
+      expect(result).toBeDefined();
+      expect(result).toBeInstanceOf(Object);
+      console.log(result);
+    });
+    it('should throw a Forbidden Exception', async() => {
+      const createUserDTO: CreateUserDTO = {
+        userID: "dummy10",
+        userName: "dummy10",
+        password: "test123",
+        role: "test"
+      };
+      try {
+        await userController.create(createUserDTO);
+      } catch (e) {
+        expect(e).toBeInstanceOf(ForbiddenException);
+        expect(e.message).toEqual('User Already Exists');
+      }
+    });
+
+  });
+  describe('findAll',()=>{
+    it('should return array of users', async()=>{
+      const users = await userController.findAll();
+      expect(users).toBeInstanceOf(Array);
+    });
+  });
+  describe('findOne',()=>{
+    it('should return an user',async()=>{
+      const user = await userController.findOne('dummy10');
+      expect(user).toBeDefined();
+    });
+
+    it('should return throw NotFoundException',async ()=>{
+      try{
+        await userController.findOne('unvalid');
+      }catch(e){
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual('User unvalid Not Found');
+      }
+    })
+  });
+
+  describe('updateUser',()=>{
+    it('should update an user', async()=>{
+      const updateUserDTO: UpdateUserDTO = {
+        userID: "dummy10",
+        userName: "dummy10",
+        password: "test",
+        role: "updated_user"
+      };
+      await userController.updateUser(
+        'dummy10',
+        updateUserDTO
+      );
+      const updated_user = await userController.findOne('dummy10');
+      expect(updated_user.role).toEqual('updated_user');
+    });
+  });
+
+  describe('deleteUser', () => {
+    it('should delete an user', async () => {
+      const lengthBeforeDelete = (await userController.findAll()).length;
+      await userController.deleteUser('dummy10');
+      const legnthAfterDelete = (await userController.findAll()).length;
+      expect(lengthBeforeDelete-legnthAfterDelete).toEqual(1);
+    });
   });
 });
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { CreateUserDTO } from './dto/create-user.dto';
+import { UpdateUserDTO } from './dto/update-user.dto';
 import { UserService } from './user.service';
 
 @Controller('user')
@@ -7,17 +8,27 @@ export class UserController {
     constructor(private readonly userService: UserService){}
 
     @Post()
-    create(@Body() createUserDTO: CreateUserDTO){
-        return this.userService.createUser(createUserDTO);
+    async create(@Body() createUserDTO: CreateUserDTO){
+        return await this.userService.createUser(createUserDTO);
     }
 
     @Get()
-    findAll(){
-        return this.userService.findAll();
+    async findAll(){
+        return await this.userService.findAll();
     }
 
-    @Get(':id')
-    findOne(@Param('id') id:string){
-        return this.userService.findOne(id);
+    @Get('/:id')
+    async findOne(@Param('id') id:string){
+        return await this.userService.findOne(id);
+    }
+
+    @Patch('/:id')
+    async updateUser(@Param('id') userID: string, @Body() updateUserData: UpdateUserDTO){
+        return await this.userService.updateUser(userID,updateUserData);
+    }
+
+    @Delete('/:id')
+    async deleteUser(@Param('id') userID:string){
+        return await this.userService.deleteUser(userID);
     }
 }
